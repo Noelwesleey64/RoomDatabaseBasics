73a090cbd866912c2c1bffc4a8aab6d78ab96b42
noelwesleey64 <noelwesley8750@gmail.com>
2023-10-18 01:40:40 +0300

    Adding Onclick Listener to our button to retreive data inserted by user in EditTexts

--- app/src/main/java/com/learning/roomdatabasebasics/MainActivity.java
+++ app/src/main/java/com/learning/roomdatabasebasics/MainActivity.java
@@ -3,6 +3,7 @@ package com.learning.roomdatabasebasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -24,6 +25,19 @@ public class MainActivity extends AppCompatActivity {
         ageEdit = findViewById(R.id.ageEdit);
         saveButton = findViewById(R.id.saveButton);
         getDataButton = findViewById(R.id.getDataButton);
+
+        //setting an Onclick Listener to our save button
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get the value the user inputs in nameEdit EditText
+                String name = nameEdit.getText().toString();
+                //get the value the user inputs in age EditText
+                String age = ageEdit.getText().toString();
+
+
+            }
+        });
 
 
 
