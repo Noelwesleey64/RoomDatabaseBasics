a4d007a0fea35a7ce3a2c6cf5af17c255a71012a
noelwesleey64 <noelwesley8750@gmail.com>
2023-10-18 01:35:37 +0300

    Creating the interface in main_activity xml file, the initializing our itemViews in MainActivity.java

--- app/src/main/java/com/learning/roomdatabasebasics/MainActivity.java
+++ app/src/main/java/com/learning/roomdatabasebasics/MainActivity.java
@@ -3,12 +3,29 @@ package com.learning.roomdatabasebasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declaring itemViews
+    EditText nameEdit;
+    EditText ageEdit;
+
+    Button saveButton, getDataButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initializing out ItemViews
+        nameEdit = findViewById(R.id.nameEdit);
+        ageEdit = findViewById(R.id.ageEdit);
+        saveButton = findViewById(R.id.saveButton);
+        getDataButton = findViewById(R.id.getDataButton);
+
+
+
     }
 }
